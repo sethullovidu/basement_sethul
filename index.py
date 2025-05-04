cca0f4475cb257f43bda6635fbc0f682d20abbfa
sethullovidu <sethullovidu77@gmail.com>
2025-05-04 18:29:28 +0530

    user inputs and comditions

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
-i=7
-print(i)
-print(int(i)*4)
+# i=7
+# print(i)
+# print(int(i)*4)
+
+# getting user inputs
+usern=input("enter your name :")
+userge=int(input("enter your age :"))
+usergpa=float(input("enter your GPA :"))
+userexp=int(input("enter your  Experiance :"))
+
+# conditions
+if usergpa>=2 and userexp>=2:
+    print("you can apply ")
+else:
+    print("rejected")
