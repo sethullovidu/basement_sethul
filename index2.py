8d30047c2ccc5fcde7b6eb08293b263366516f38
sethullovidu <sethullovidu77@gmail.com>
2025-05-09 20:19:07 +0530

    Update index2.py
    creating person dictionary

--- index2.py
+++ index2.py
@@ -4,5 +4,8 @@ while True:
     fname=input("enter your first name :")
     lname=input("enter your last name :")
     age=int(input("enter your age :"))
-
+    person={
+        'Name':fname+" "+lname,
+        'Age':age
+        }
 
