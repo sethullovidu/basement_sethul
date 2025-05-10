064b0f9f7321addc201e6cd7ebed2ad37cdeac91
sethullovidu <sethullovidu77@gmail.com>
2025-05-10 16:49:23 +0530

    Update index3.py
    creating infinity user inputs

--- index3.py
+++ index3.py
@@ -1,1 +1,7 @@
-
+nlist=[]
+while True :
+    fname=input(" enter your first name :")
+    fname=input(" enter your first name :")
+    age=input("enter your age :")
+    gpa=int(input("enter your GPA :"))
+    
