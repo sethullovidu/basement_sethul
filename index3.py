2b0f720a22251a75600a0ec915c912ac4beb24f0
sethullovidu <sethullovidu77@gmail.com>
2025-05-12 11:24:27 +0530

    Update index3.py
    important

--- index3.py
+++ index3.py
@@ -48,7 +48,12 @@ print(cperson)
 # # Removing duplicates from  List
 # creating list
 mylist = ["a", "b", "a", "c", "c"]
-# removing duplicates
+#  removing duplicates
 mylist = list( dict.fromkeys(mylist) )
 # displaying none duplicates list
 print(mylist)
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
