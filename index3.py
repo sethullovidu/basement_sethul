e244e524298f78ca7076100d67371103eb138390
sethullovidu <sethullovidu77@gmail.com>
2025-05-14 22:35:07 +0530

    Update index3.py

--- index3.py
+++ index3.py
@@ -57,3 +57,4 @@ print(mylist)
 # Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
 # Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
 # Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
