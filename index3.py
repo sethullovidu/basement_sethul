1f1a09868ee0e11ec7dae4a25181b7337e3e1165
sethullovidu <sethullovidu77@gmail.com>
2025-05-14 22:36:47 +0530

    reversing a string

--- index3.py
+++ index3.py
@@ -58,3 +58,6 @@ print(mylist)
 # Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
 # Dictionary is a collection which is ordered** and changeable. No duplicate members.
 
+# reversing a string
+txt = "Hello World"[::-1]
+print(txt)
