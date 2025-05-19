e623a74bebe9fc8d174535fa5e8677ab0f09a0b9
sethullovidu <sethullovidu77@gmail.com>
2025-05-19 13:03:04 +0530

    Update index4.py

--- index4.py
+++ index4.py
@@ -7,4 +7,5 @@ import numpy as np
 
 # creating student array
 students = np.array([1, 2, 3, 4, 5, 6, 7, 8])
-
+# creating avarage mark of students array
+avgmarks = np.array([50, 60, 70, 11, 22, 54, 49, 51]) 
