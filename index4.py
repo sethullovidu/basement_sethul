dd7bb18782be8e473d1eeaaf9f6f8b098e677c75
sethullovidu <sethullovidu77@gmail.com>
2025-05-19 13:01:32 +0530

    Update index4.py

--- index4.py
+++ index4.py
@@ -1,5 +1,10 @@
-# visualization line graph
+#---- visualization line graph----
 
 # importing matplotlibpyplot 
 import matplotlib.pyplot as plt
 # importing numpy
+import numpy as np
+
+# creating student array
+students = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
