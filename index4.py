ad91774d2947b8ac239d3074115de2a2acd292dd
sethullovidu <sethullovidu77@gmail.com>
2025-05-19 13:05:51 +0530

    Update index4.py

--- index4.py
+++ index4.py
@@ -4,8 +4,9 @@
 import matplotlib.pyplot as plt
 # importing numpy
 import numpy as np
-
 # creating student array
 students = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 # creating avarage mark of students array
 avgmarks = np.array([50, 60, 70, 11, 22, 54, 49, 51]) 
+# visualising as a bar plot
+plt.bar(students,avgmarks)
