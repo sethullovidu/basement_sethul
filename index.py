07f15b0ab353b26bfdd7260516c20a8d5d5354ee
sethullovidu <sethullovidu77@gmail.com>
2025-06-23 20:30:21 +0530

    Update index.py

--- index.py
+++ index.py
@@ -141,9 +141,4 @@ while True:
     nlist.append(luser)
 # displying list after loop breaks
 print("Names entered:", nlist)
-print("Names entered:", nlist)
-print("Names entered:", nlist)
-print("Names entered:", nlist)
-print("Names entered:", nlist)
-print("Names entered:", nlist)
-print("Names entered:", nlist)
+
